3da09b43a0beadf3211aa80988a1332ed79807d9
alaminakash810 <alaminakash810@gmail.com>
2025-02-22 21:09:41 +0600

    basic output using js

---
+++ basic_output.js
@@ -0,0 +1,21 @@
+console.log("Hi there!");
+console.log("i have everything");
+
+//window.alert("This has been created to alert the user"); //this is a alert function
+
+document.getElementById("hid1").textContent = "Work hard";
+document.getElementById("pid1").textContent = "lorem ipsum dolor sit amet, consectetur adipiscing elit  erat, sed diam  euismod er";
+
+let fullname = "Al Amin";
+let age = 22;
+let student = true;
+
+document.getElementById("pid2").textContent = `My full name is ${fullname}`;
+document.getElementById("pid3").textContent = `I am ${age} years old`;
+document.getElementById("pid4").textContent = `I am a full time student at IUB: ${student}`;
+
+
+// let username;
+// username = window.prompt("What's your username?");
+// console.log(username);
+// document.getElementById("hid2").textContent = `username is ${username}`;
