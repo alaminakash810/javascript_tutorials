969c3e371020818adee8429acb8a7959838f70aa
alaminakash810 <alaminakash810@gmail.com>
2025-02-22 21:53:27 +0600

    strict equality operator

---
+++ strict_equality_op.js
@@ -0,0 +1,28 @@
+//  = assignment operator
+//  == comparison operator (compare if values are equal)
+// === strict equality operator (compare if values and datatypes are equal)
+// != inequality operator
+// !== strict inequality operator
+
+const PI = 3.1416;
+
+if (PI === 3.1416) {
+    console.log("That is Pi")   
+}
+else {
+    console.log("That is NOT Pi");
+}
+
+if (PI != 3.1416) {
+    console.log("That is NOT Pi")   
+}
+else {
+    console.log("That is Pi");
+}
+
+if (PI !== 3.1416) {
+    console.log("That is NOT Pi")   
+}
+else {
+    console.log("That is Pi");
+}
