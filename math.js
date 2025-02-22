d2c50572f0e4ac3c474411f6e97b27639f25b9b8
alaminakash810 <alaminakash810@gmail.com>
2025-02-22 19:10:58 +0600

    math functions in js

---
+++ math.js
@@ -0,0 +1,31 @@
+// Math = built in object that provides a 
+//        collection of properties and methods
+
+let a = 3.1416;
+let b = 2.38;
+let c = 1;
+let f = 9;
+let g = 45;
+
+// d = Math.round(a); to round a number
+// d = Math.floor(b); floor method
+// d = Math.ceil(b); ceil method
+// d = Math.trunc(b); removes the decimal point
+// d = Math.pow(f, c); f to the power of c
+// d = Math.sqrt(f); 
+// d = Math.log(a); log method
+// d = Math.abs(b); absolute method
+// d = Math.sin(g); trigonometric method
+// d = Math.cos(g); trigonometric method
+// d = Math.tan(g); trigonometric method
+// d = Math.sign(g); to check the sign of a number
+// d = Math.log(g); to check the logarithm of a number
+// d = Math.log2(g); to check the base 2 logarithm of a number
+// d = Math.log10(g); to check the base 10 logarithm of a number
+
+
+let max = Math.max(a, b, c, f);
+let min = Math.min(a, b, c, f);
+
+console.log(max);
+console.log(min);
