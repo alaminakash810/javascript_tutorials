c24ccd06588b7d34453624f92095b58e1c5779af
alaminakash810 <alaminakash810@gmail.com>
2025-02-22 17:25:27 +0600

    arithmetic operations in js

---
+++ arithmeticop.js
@@ -0,0 +1,42 @@
+// arithmetic operators = operands (values, variables, etc)
+//                         operators = + _ * / 
+
+let student = 31;
+
+// student = student + 1;
+// student = student -1;
+// student = student * 2;
+// student = student / 2;
+// student = student ** 2; for exponentials
+let isstudent = student % 2; //for remainder 
+
+
+//console.log(student);
+console.log(isstudent);
+
+// augmented assignment operators
+// student += 1;
+// student -= 1;
+// student *= 2;
+// student /= 2;
+// student **= 2;
+// student %= 2;
+
+// incremental and decremental assignment operators
+// student ++;
+student --;
+
+console.log(student);
+
+/*
+
+ operator precedence
+ 1.paranthesis
+ 2.exponents
+ 3.multiplications & divisions & modulus
+ 4.addition and subtraction
+
+ */
+
+ let result = 2 + 3 * 2 - 1 + 2**3
+ console.log(result);
