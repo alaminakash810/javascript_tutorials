5950372deaf9f913854de151eb18bb4ea61da1f2
alaminakash810 <alaminakash810@gmail.com>
2025-02-22 16:18:15 +0600

    variables in javascript

---
+++ variable.js
@@ -0,0 +1,27 @@
+// variable = A container that stores a value.
+//            Behaves as if it were the it contains.
+
+let x;
+x = 50;
+let y = 40;
+console.log(y);
+console.log(x);
+
+let age = 22;
+let price = 20.99;
+let gpa = 3.11;
+console.log(`My age is ${age} and the price of the book is $${price}`);
+console.log(`My cgpa is ${gpa}`);
+
+console.log(typeof age); //for data types
+
+let firstname="Al Amin";
+console.log(`My first name is ${firstname}`);
+console.log(typeof firstname);
+
+//boolean true or false
+let online = true;
+let summer = false;
+console.log(typeof summer);
+console.log(`The current season is summer: ${summer}`);
+console.log(`${firstname} is online ${online}`);
